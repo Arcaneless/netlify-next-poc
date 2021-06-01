b6c2ada0a0682b0ef9c0e1b334033368249f33ec
Arcaneless <kings88819@gmail.com>
2021-06-01 12:53:04 +0800

    remove image

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,4 @@
 import Head from 'next/head'
-import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import { attributes, react as HomeComponent } from '../content/home.md'
 import { useMemo } from 'react'
